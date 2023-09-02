f8e65efc64df4253c3dcf49d44b6ed8bb1ea5d63
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2023-09-02 15:38:32 +0800

    Add tab index to play button

--- components/ui/play-button.tsx
+++ components/ui/play-button.tsx
@@ -2,10 +2,18 @@
 
 import { Button } from "@/components/ui/button";
 import { Triangle } from "@/components/ui/triangle";
+import { cn } from "@/lib/utils";
 
 export function PlayButton() {
   return (
-    <Button className="absolute right-4 h-12 w-12 rounded-full p-3 text-white opacity-0 transition-all duration-200 ease-in-out hover:scale-105 group-hover:opacity-100">
+    <Button
+      onClick={() => alert("click")}
+      tabIndex={0}
+      className={cn(
+        "absolute right-4 h-12 w-12 rounded-full p-3 text-white opacity-0 " +
+          "transition-all duration-200 ease-in-out hover:scale-105 group-hover:opacity-100 group-focus:opacity-100 focus:opacity-100"
+      )}
+    >
       <Triangle />
     </Button>
   );
