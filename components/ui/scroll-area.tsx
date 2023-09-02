d03d0de8f6fd13d8f674e7c00e3c67a2bd30426a
Alcedo Nathaniel De Guzman Jr <natealcedo@gmail.com>
2023-09-02 19:46:32 +0800

    Add group scroll for namespacing in scroll area

--- components/ui/scroll-area.tsx
+++ components/ui/scroll-area.tsx
@@ -11,7 +11,7 @@ const ScrollArea = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
-    className={cn("relative overflow-hidden group", className)}
+    className={cn("relative overflow-hidden group/scroll", className)}
     {...props}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
@@ -38,7 +38,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-transparent transition-colors duration-500 ease-in-out group-hover:bg-[#b3b3b3]/50" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-transparent transition-colors duration-500 ease-in-out group-hover/scroll:bg-[#b3b3b3]/50" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
